9c210582622b80bc094fa3d29dde73cd9fcca549
dannytayade1495 <dannytayade1495@gmail.com>
2023-09-12 13:45:05 +0530

    ADD feature - REST

--- springrest/src/main/java/com/jspiders/springrest/controller/StudentController.java
+++ springrest/src/main/java/com/jspiders/springrest/controller/StudentController.java
@@ -2,15 +2,18 @@ package com.jspiders.springrest.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.jspiders.springrest.pojo.StudentPOJO;
+import com.jspiders.springrest.response.StudentResponse;
 import com.jspiders.springrest.service.StudentService;
 
-@Controller
+@RestController
 public class StudentController {
 	
 	@Autowired
@@ -19,13 +22,14 @@ public class StudentController {
 	@PostMapping(path = "/add",
 			consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public void addStudent(@RequestBody StudentPOJO pojo) {
+	public ResponseEntity<StudentResponse> addStudent(@RequestBody StudentPOJO pojo) {
 		StudentPOJO student = service.addStudent(pojo);
 		//SUCCESS
 		if (student != null) {
-			
+			return new ResponseEntity<StudentResponse>(new StudentResponse("Data added successfully. ", student, null), HttpStatus.ACCEPTED);
 		}
 		//FAILURE
+		return new ResponseEntity<StudentResponse>(new StudentResponse("Data not added. ", null, null), HttpStatus.ACCEPTED);
 	}
 
 }
--- springrest/src/main/java/com/jspiders/springrest/response/StudentResponse.java
+++ springrest/src/main/java/com/jspiders/springrest/response/StudentResponse.java
@@ -1,5 +1,22 @@
 package com.jspiders.springrest.response;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.jspiders.springrest.pojo.StudentPOJO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StudentResponse {
+	
+	private String msg;
+	private StudentPOJO student;
+	private List<StudentPOJO> students;
 
 }
